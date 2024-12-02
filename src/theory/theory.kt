846733c21669dd9d4a54b4c24d043032254227ca
JamirGDC <gutierrez.dc@icloud.com>
2024-12-02 13:43:00 +0100

    exercise Numbers complete  12/02/2024

---
+++ src/theory/theory.kt
@@ -0,0 +1,59 @@
+package theory////datos
+//
+//val integer: Int = 1;
+//val string: String = "test";
+//val char: Char = 'c';
+//val bool: Boolean = true;
+//val double: Double = 1.0;
+//val float: Float = 1.0F;
+//val long: Long = 1L;
+//val short: Short = 1;
+//val byte: Byte = 1;
+//
+//
+//
+//println("Hello World!")
+//
+//// 2 Bucles
+//    for (i in 0..5)
+//        {
+//println("Value is $i");
+//    }
+//            for (i in 0..5 step 2)
+//        {
+//println("Value is $i");
+//    }
+//
+//var valA = 3;
+//var valB = 5;
+//
+//    while(valA <= valB)
+//        {
+//println("hola soy pepe");
+//valA++;
+////        valA.inc();
+//
+//
+//        }
+//
+//        do
+//        {
+//println("hola soy pepe");
+//valA++;
+//        }while (valA < valB);
+//
+//var day = 10;
+//
+//
+//
+//when(day)
+//{
+//    1 -> println("monday");
+//    2 -> println("tuesday");
+//    3 -> println("wednesday");
+//    4 -> println("thursday");
+//    5 -> println("friday");
+//    6 -> println("saturday")
+//    7 -> println("sunday");
+//        else -> println("no has ingresado un dia valido");
+//}
