1b3207ce21523dabebc1db105586a363e5c9d21b
JamirGDC <gutierrez.dc@icloud.com>
2024-12-03 14:19:01 +0100

    fix float percentage

--- src/excerciseNumbers/ExerciseNumbersMethods.kt
+++ src/excerciseNumbers/ExerciseNumbersMethods.kt
@@ -4,12 +4,12 @@ class ExerciseNumbersMethods {
 
     fun sum(firstNumber: Int, secondNumber: Int): Int
     {
-        return firstNumber + secondNumber;
+        return firstNumber + secondNumber
     }
 
     fun multiply(firstNumber: Int, secondNumber: Int): Int
     {
-        return firstNumber * secondNumber;
+        return firstNumber * secondNumber
     }
     fun divide(dividend: Double, divider: Int): Double? {
         return if (divider != 0) dividend / divider else null
@@ -18,42 +18,42 @@ class ExerciseNumbersMethods {
     {
         return if(dividend != 0) divider else null
     }
-    fun percentage(playedMatch: Int, wonMatches: Int): String?
+    fun percentage(playedMatches: Int, wonMatches: Int): String?
     {
-        if (playedMatch < wonMatches) return null
-        val result = playedMatch * 100 / wonMatches;
-        return "$result%";
+        if (playedMatches < wonMatches) return null
+        val result : Float = wonMatches * 100f / playedMatches
+        return "$result%"
     }
     fun largestNumber(firstNumber: Int, secondNumber: Int): Int
     {
-        return maxOf(firstNumber, secondNumber);
+        return maxOf(firstNumber, secondNumber)
     }
     fun volume(with: Int, volume: Int, height: Int): Int
     {
-        return with * volume * height;
+        return with * volume * height
     }
     fun primeNumber(primeCount: Int): List<Int>
     {
         val numbers: MutableList<Int> = mutableListOf()
-        var firstPrimeNumber = 2;
+        var firstPrimeNumber = 2
 
         while (numbers.size < primeCount)
         {
             if (isPrimeNumber(firstPrimeNumber)) numbers.add(firstPrimeNumber)
             firstPrimeNumber++
         }
-        return numbers;
+        return numbers
     }
 
     private fun isPrimeNumber(checkNumber: Int): Boolean
     {
-        if(checkNumber == 0 || checkNumber == 1 || checkNumber == 4) return false;
+        if(checkNumber == 0 || checkNumber == 1 || checkNumber == 4) return false
         for (i in 2..<checkNumber)
         {
             if(checkNumber % i == 0)
-                return false;
+                return false
         }
-        return true;
+        return true
     }
 
 
--- src/excerciseNumbers/exerciseNumbers.kt
+++ src/excerciseNumbers/exerciseNumbers.kt
@@ -1,31 +1,33 @@
 package excerciseNumbers
 
 fun main() {
-    val exercisesNumbers = ExerciseNumbersMethods();
+    val exercisesNumbers = ExerciseNumbersMethods()
 
-    val exercise1 : Int =  exercisesNumbers.sum(2, 3)
-    println("La suma es $exercise1")
+    val exerciseNumbers = ExerciseNumbersMethods()
 
-    val exercise2 : Int =  exercisesNumbers.multiply(2, 3)
-    println("La multiplicacion es $exercise2")
+    val exercise1: Int = exerciseNumbers.sum(2, 3)
+    println("The sum is: $exercise1")
 
-    val exercise3 : Double? =  exercisesNumbers.divide(2.4, 3)
-    println("La division es $exercise3")
+    val exercise2: Int = exerciseNumbers.multiply(2, 3)
+    println("The multiplication result is: $exercise2")
 
-    val exercise4 : Int? =  exercisesNumbers.divide(2, 3)
-    println("El divisor es $exercise4")
+    val exercise3: Double? = exerciseNumbers.divide(2.4, 3)
+    println("The division result is: $exercise3")
 
-    val exercise5 : String? =  exercisesNumbers.percentage(4, 3)
-    println("El porcentaje es $exercise5")
+    val exercise4: Int? = exerciseNumbers.divide(2, 3)
+    println("The division result is: $exercise4")
 
-    val exercise6 : Int =  exercisesNumbers.largestNumber(2, 3)
-    println("El numero mas grande es $exercise6")
+    val exercise5: String? = exerciseNumbers.percentage(99, 7)
+    println("The percentage is: $exercise5")
 
-    val exercise7 : Int =  exercisesNumbers.volume(2, 3, 3)
-    println("El volumen es $exercise7")
+    val exercise6: Int = exerciseNumbers.largestNumber(2, 3)
+    println("The largest number is: $exercise6")
 
-    val exercise8 : List<Int> =  exercisesNumbers.primeNumber(5)
-    println("Los numeros primos son $exercise8")
+    val exercise7: Int = exerciseNumbers.volume(2, 3, 3)
+    println("The volume is: $exercise7")
+
+    val exercise8: List<Int> = exerciseNumbers.primeNumber(5)
+    println("The prime numbers are: $exercise8")
 
 
 }
